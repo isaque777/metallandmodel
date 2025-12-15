a1d86b3b78658accf377ce485151044fdee01af3
Isaque Soares <isaque777@gmail.com>
2025-12-15 15:48:39 -0500

    Refactor Memo class structure and add new properties for enhanced functionality

--- src/app/models/memo.ts
+++ src/app/models/memo.ts
@@ -1,12 +1,29 @@
 import { RelationFile } from './relationFile';
 
 export class Memo {
-  id: number = 0;
-  title: string = '';
+  date: Date = new Date();
   description: string = '';
-  subTitle: string = '';
-  memoFiles: RelationFile[] = [];
+  descriptionEn?: string;
+  descriptionNonHtml?: string;
+  descriptionNonHtmlProcessed?: Date;
+  description_html: string = '';
+  elasticSearchProcessed?: Date;
+  englishText: boolean = false;
+  externalUserEmail?: string;
+  externalUserName?: string;
+  forApproval: boolean = false;
   highlight: boolean = false;
   html: boolean = false;
-  description_html: string = '';
+  id: number = 0;
+  image?: Uint8Array;
+  memoFiles: RelationFile[] = [];
+  memoUserSelected?: any; // Userz type, replace with actual type if available
+  status: string = 'ACTIVE'; // Status enum, use string or replace with enum if defined
+  subTitle: string = '';
+  title: string = '';
+  titleEn?: string;
+  translationProcessed?: Date;
+  views: number = 0;
+  viewsToday: number = 0;
+  viewsTodayDate: Date = new Date();
 }
